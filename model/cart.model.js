813899239ad55ce56ed36a02777a0a3579f35d2a
Yuri <you@example.com>
2025-12-18 20:43:38 +0500

    Прописать модель item в items для cart.model и расчет общего количества заказов и общая сумма заказа

--- model/cart.model.js
+++ model/cart.model.js
@@ -8,7 +8,35 @@ const cartSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    items: [],
+    items: [{
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+        title: {
+        type: String,
+        required: true
+        },
+        inStock: {
+        type: Boolean,
+        default: true
+        },
+        addedAt: {
+        type: Date,
+        default: Date.now
+        }
+    }],
     totalPrice: {
         type: Number,
         default: 0,
@@ -21,6 +49,22 @@ const cartSchema = new mongoose.Schema({
     }
 }, {
     timestamps: true
+});
+
+cartSchema.pre('save', function(next) {
+    try {
+        if (!this.items || this.items.length === 0) {
+            this.totalPrice = 0
+            this.totalQuantity = 0
+            return next()
+        }
+        this.totalQuantity = this.items.reduce((sum, item) => sum + item.quantity, 0);
+        this.totalPrice = this.items.reduce((sum, item) => sum + (item.price * item.quantity), 0);
+        next();
+    } catch (error) {
+        console.log("Ошибка при расчете корзины", error);
+        next(error)
+    }
 });
 
 export const Cart = mongoose.model("Cart", cartSchema);
